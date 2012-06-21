8e2a4682ed23e33d88e54aaf72355af4e9efb0e4
Alan Gorton <dev.code1up@googlemail.com>
2012-06-21 21:14:56 +0100

    wip

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-// Hello
+var express = require("express");
+
+var app = express.createServer();
+
+app.get("/", function(req, res) {
+	res.send("Hello, World.");
+});
+
+app.listen(process.env.PORT || 3000);
 

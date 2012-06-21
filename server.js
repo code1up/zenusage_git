2e32ba1b86a0d52ab8a520a5c3ef9000e3291b71
Alan Gorton <dev.code1up@googlemail.com>
2012-06-21 22:48:16 +0100

    wip

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var ws = require("./zenws/usagews.js");
 
 var app = express.createServer();
 
@@ -6,5 +7,32 @@ app.get("/", function(req, res) {
 	res.send("Hello, World.");
 });
 
-app.listen(process.env.PORT || 3000);
+app.get("/api/signin/:username/:password", function(req, res, next) {
+    var username = req.params.username;
+    var password = req.params.password;
 
+    console.log(username);
+    console.log(password);
+
+    res.contentType("text/json");
+
+    /*
+    res.send({
+        username: username,
+        password: password
+    });
+    */
+    
+    // username = "alanjgorton@googlemail.com";
+    // password = "p4ssword";
+
+    ws.signin(username, password, function(error, data) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+app.listen(process.env.PORT || 3000);
